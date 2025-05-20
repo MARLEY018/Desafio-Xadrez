22aaf9969ab432f4c52bbc7ab4b8d429bd35268e
MARLEY018 <marleyantunes018@gmail.com>
2025-05-19 21:28:32 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include "tabuleiro.h"
 
+int converterColuna(char c) {
+    return c - 'a';
+}
+
+int main() {
+    char tabuleiro[8][8];
+    inicializarTabuleiro(tabuleiro);
+    mostrarTabuleiro(tabuleiro);
+
+    char colOrig, colDest;
+    int linOrig, linDest;
+
+    while (1) {
+        printf("\nMover peça (ex: e2 e4): ");
+        scanf(" %c%d %c%d", &colOrig, &linOrig, &colDest, &linDest);
+
+        // Converte para índices do array
+        int iOrig = 8 - linOrig;
+        int jOrig = converterColuna(colOrig);
+        int iDest = 8 - linDest;
+        int jDest = converterColuna(colDest);
+
+        if (moverPeça(tabuleiro, iOrig, jOrig, iDest, jDest)) {
+            mostrarTabuleiro(tabuleiro);
+        } else {
+            printf("Movimento inválido!\n");
+        }
+    }
+
+    return 0;
+}
