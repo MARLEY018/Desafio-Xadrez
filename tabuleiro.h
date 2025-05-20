cb33a431c551fe71b8591b21b6af1587defc48a2
MARLEY018 <marleyantunes018@gmail.com>
2025-05-19 21:30:05 -0300

    Add files via upload

--- tabuleiro.h
+++ tabuleiro.h
@@ -1,1 +1,8 @@
+#ifndef TABULEIRO_H
+#define TABULEIRO_H
 
+void inicializarTabuleiro(char tabuleiro[8][8]);
+void mostrarTabuleiro(char tabuleiro[8][8]);
+int moverPeça(char tabuleiro[8][8], int linOrig, int colOrig, int linDest, int colDest);
+
+#endif
